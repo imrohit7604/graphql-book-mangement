466757d05b0d8a34e3c2d78b033d8ee9066cf171
Mr . No One <imrohit7604@gmail.com>
2021-05-26 15:38:52 +0530

    Dockeriziation and compose file running fine

--- server/app.js
+++ server/app.js
@@ -5,7 +5,17 @@ const mongoose=require("mongoose")
 const cors=require("cors")
 const app=express()
 
+/*
+//use this if mongodb is runninng in your local system
 mongoose.connect("mongodb://localhost:27017/local",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }).then(()=>console.log("Connected to DB"))*/
+
+  //Use this if mongoDB is running in docker change localhost/book-mongo to your mongo image service name
+  mongoose.connect("mongodb://book-mongo:27017/local",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
